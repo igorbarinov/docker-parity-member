949df1ff7372f141adfa689c326c25bb74987af3
Igor Barinov <igorbarinov@me.com>
2016-09-06 23:59:44 -0700

    light version

--- configure-parity.sh
+++ configure-parity.sh
@@ -1,18 +1,13 @@
   
 #!/bin/bash
 
-echo "home: $HOME"
-echo "user: $(whoami)"
-
-
-
 #####################
 # create an account #
 #####################
+
 PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1)
 echo $PASSWORD > $HOME/.parity-pass
 
 parity account new --password $HOME/.parity-pass
 
 address=0x$(parity account list | awk 'END{print}' | tr -cd '[:alnum:]._-')
-
